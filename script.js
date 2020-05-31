926f6684d336f7cd7ad14570b740149b6afe97ba
Tim Fredriksson <timtimmen@protonmail.ch>
2020-05-31 13:06:20 +0200

    I don't know

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function viewAnswer() {
   var powerbank = document.getElementById("powerbankInput").value;
   var device = document.getElementById("deviceInput").value;
   var outpt = powerbank / device * 0.7;
-  
+
   document.getElementById("output").innerHTML = "Du kan ladda din enhet" + " " + outpt + " " + "Gång/er från denna powerbank!"
-  
+
   if (powerbank > 27800) {
     document.getElementById("outputNumberTwo").innerHTML = "Men du får inte ta med din powerbank på flyget."
   }else {
-    document.getElementById("outputNumberTwo").innerHTML = "Du har tur, du får ta med dig din powerbank på flyget."
+    document.getElementById("outputNumberTwo").innerHTML = "Och du har tur, du får ta med dig din powerbank på flyget."
   }
 }
